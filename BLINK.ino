ffb7f85e15d7c549cadb8ea81cfa5c4d7d353620
IoT withme <82151064+IoTWM@users.noreply.github.com>
2021-04-28 14:49:43 +0530

    Update BLINK.ino

--- BLINK.ino
+++ BLINK.ino
@@ -9,22 +9,26 @@
  **********************************************************************************/
 
 
-#define BLYNK_PRINT Serial
+#define BLYNK_PRINT Serial  
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-char auth[] = "ITCU-Xs1oDI5j9belA0idU4m4RFco6LJ";
-char ssid[] = "abcdefgh";
-char pass[] = "12345678";
+
+char auth[] = "ITCU-Xs1oDI5j9belA0idU4m4RFco6LJ";              //Authorisation key
+char ssid[] = "abcdefgh";                                      //SSID
+char pass[] = "12345678";                                      //Password
+
 void setup()
 {
   Serial.begin(9600);
   Blynk.begin(auth, ssid, pass);
 }
-BLYNK_WRITE(5)
+
+BLYNK_WRITE(4)                                                 //D2- GOIO04
 {
   int pinvalue=param.asInt();
   Serial.println(pinvalue);
 }
+
 void loop()
 {
   Blynk.run();
